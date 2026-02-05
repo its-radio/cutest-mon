b3f0d1d0c1b98d9cb06e293a12740263cbb6431c
radio <email@email.com>
2026-02-05 10:33:51 -0500

    add global background styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 export default function Home() {
-  return <div className="text-2xl text-red-200">Hello World</div>
+  return <div className="">Hello World</div>
 }
