6a0dd82841a5d4ee8ab3a394d4bd4c4a8d00be5a
radio <email@email.com>
2026-02-08 20:53:47 -0500

    add ssr

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -5,3 +5,6 @@ export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: () => ({}),
 })
+
+export type { AppRouter } from '@/backend/router'; // re-export AppRouter type because tRPC docs say so.
+                                                   // keeps type export co-located with API endpoint
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,7 +1,10 @@
 import { createTRPCNext } from '@trpc/next';
 import { httpBatchLink } from '@trpc/client';
-import type { AppRouter } from '@/backend/router';
+// import type { AppRouter } from '@/backend/router';
+import type { AppRouter } from '@/pages/api/trpc/[trpc]';
 
+
+/*
 export const trpc = createTRPCNext<AppRouter>({
     config(ctx) {
         return {
@@ -18,4 +21,61 @@ export const trpc = createTRPCNext<AppRouter>({
         };
     },
   ssr: false,
+});
+
+*/
+
+//import { httpBatchLink } from '@trpc/client';
+//import { createTRPCNext } from '@trpc/next';
+import { ssrPrepass } from '@trpc/next/ssrPrepass';
+import superjson from 'superjson';
+// import type { AppRouter } from '@/pages/api/trpc/[trpc]';
+
+function getBaseUrl() {
+    if (process.browser) return ""; // browser should use current path
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // ssr should use vercel url
+    return `https://localhost:${process.env.PORT ?? 3000}`; //dev ssr should use localhost
+
+}
+
+
+
+export const trpc = createTRPCNext<AppRouter>({
+  ssr: true,
+  ssrPrepass,
+  config(config) {
+    const { ctx } = config;
+    if (typeof window !== 'undefined') {
+      // during client requests
+      return {
+        links: [
+          httpBatchLink({
+            url: '/api/trpc',
+          }),
+        ],
+      };
+    }
+    return {
+      links: [
+        httpBatchLink({
+          // The server needs to know your app's full url
+          url: `${getBaseUrl()}/api/trpc`,
+          /**
+           * Set custom request headers on every request from tRPC
+           * @see https://trpc.io/docs/v10/header
+           */
+          headers() {
+            if (!ctx?.req?.headers) {
+              return {};
+            }
+            // To use SSR properly, you need to forward client headers to the server
+            // This is so you can pass through things like cookies when we're server-side rendering
+            return {
+              cookie: ctx.req.headers.cookie,
+            };
+          },
+        }),
+      ],
+    };
+  },
 });
